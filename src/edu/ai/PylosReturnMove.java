90a5f3586e8183cc417a4a29473ea8e5c0458dfd
EaTea <ed.r.tay@gmail.com>
2012-05-27 23:32:40 +0800

    ReturnMove: Uses arraylist to handle removals now

--- src/edu/ai/PylosReturnMove.java
+++ src/edu/ai/PylosReturnMove.java
@@ -1,17 +1,20 @@
 package edu.ai;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PylosReturnMove extends PylosRaiseMove {
 
-	PylosPosition[] removals;
+	List<PylosPosition> removals;
 	
 	public PylosReturnMove(PylosPosition move, PylosPosition raiseFrom, PylosPosition... removals) {
 		super(move,raiseFrom);
-		this.removals = new PylosPosition[2];
+		this.removals = new ArrayList<PylosPosition>();;
 		if(removals.length > 2) {
 			throw new IllegalArgumentException("Attempted to remove more than two spheres");
 		}
 		for(int i = 0; i < removals.length; i++) {
-			this.removals[i] = removals[i];
+			this.removals.add(removals[i]);
 		}
 	}
 }
