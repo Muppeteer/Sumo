eb1e8e5425c1aa07ae436ea5223d8be427d6f92a
EaTea <ed.r.tay@gmail.com>
2012-05-27 23:32:06 +0800

    Pos: fix bug in position with stupid checks in setters
    TODO: Refit to use nice checks

--- src/edu/ai/PylosPosition.java
+++ src/edu/ai/PylosPosition.java
@@ -6,22 +6,22 @@ package edu.ai;
  */
 public final class PylosPosition {
 
-	int colour;
+	PylosColour colour;
 	int x; //position horizontally
 	int y; //position vertically in plane
 	int z; //position depthwise
 	
-	public PylosPosition(int x, int y, int z, int colour) {
+	public PylosPosition(int x, int y, int z, PylosColour colour) {
 		setZ(z);
 		setY(y); setX(x);
 		setColour(colour);
 	}
 
-	public int getColour() {
+	public PylosColour getColour() {
 		return colour;
 	}
 	
-	private void setColour(int colour) {
+	private void setColour(PylosColour colour) {
 		this.colour = colour;
 	}
 
@@ -40,24 +40,28 @@ public final class PylosPosition {
 	private void setX(int x) {
 		switch(z) {
 			case(0): {
-				if(x >= 3 || x < 0) {
+				if(x > 3 || x < 0) {
 					throw new IllegalArgumentException("Tried to set x to " + x + ", expected to be within [0,3]");
 				}
+				break;
 			}
 			case(1): {
-				if(x >= 2 || x < 0) {
+				if(x > 2 || x < 0) {
 					throw new IllegalArgumentException("Tried to set x to " + x + ", expected to be within [0,2]");
 				}
+				break;
 			}
 			case(2): {
-				if(x >= 1 || x < 0) {
+				if(x > 1 || x < 0) {
 					throw new IllegalArgumentException("Tried to set x to " + x + ", expected to be within [0,1]");
 				}
+				break;
 			}
 			case(3): {
 				if(x != 0) {
 					throw new IllegalArgumentException("Tried to set x to " + x + ", expected to be 0");
 				}
+				break;
 			}
 			default:
 				break;
@@ -68,24 +72,28 @@ public final class PylosPosition {
 	private void setY(int y) {
 		switch(z) {
 			case(0): {
-				if(y >= 3 || y < 0) {
+				if(y > 3 || y < 0) {
 					throw new IllegalArgumentException("Tried to set y to " + y + ", expected to be within [0,3]");
 				}
+				break;
 			}
 			case(1): {
-				if(y >= 2 || y < 0) {
+				if(y > 2 || y < 0) {
 					throw new IllegalArgumentException("Tried to set y to " + y + ", expected to be within [0,2]");
 				}
+				break;
 			}
 			case(2): {
-				if(y >= 1 || y < 0) {
+				if(y > 1 || y < 0) {
 					throw new IllegalArgumentException("Tried to set y to " + y + ", expected to be within [0,1]");
 				}
+				break;
 			}
 			case(3): {
 				if(y != 0) {
 					throw new IllegalArgumentException("Tried to set y to " + y + ", expected to be 0");
 				}
+				break;
 			}
 			default:
 				break;
