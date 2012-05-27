fb45231be8e2057561a9c51c69e116226104c584
EaTea <ed.r.tay@gmail.com>
2012-05-27 12:10:42 +0800

    Spheres: Change representation of SphereColour

--- src/edu/ai/PylosEnvironment.java
+++ src/edu/ai/PylosEnvironment.java
@@ -5,20 +5,20 @@ import java.util.List;
 
 public class PylosEnvironment {
 
-	private SphereColour[][][] boardRep;
-	private SphereColour currentPlayer;
+	private int[][][] boardRep;
+	private int currentPlayer;
 	
 	public PylosEnvironment() {
 		//WHITE is first to play
 		currentPlayer = SphereColour.WHITE;
-		boardRep = new SphereColour[4][][];
-		boardRep[0] = new SphereColour[4][4];
-		boardRep[1] = new SphereColour[3][3];
-		boardRep[2] = new SphereColour[2][2];
-		boardRep[3] = new SphereColour[1][1];
+		boardRep = new int[4][][];
+		boardRep[0] = new int[4][4];
+		boardRep[1] = new int[3][3];
+		boardRep[2] = new int[2][2];
+		boardRep[3] = new int[1][1];
 		//ensure that BoardPositions begin as Empty
-		for(SphereColour[][] a : boardRep) {
-			for(SphereColour[] b : a) {
+		for(int[][] a : boardRep) {
+			for(int[] b : a) {
 				Arrays.fill(b, SphereColour.EMPTY);
 			}
 		}
@@ -27,7 +27,7 @@ public class PylosEnvironment {
 	//note: following checker methods assume that they were checked beforehand,
 	//will die if not so
 	private boolean isEmpty(int x, int y, int z) {
-		return boardRep[z][x][y].equals(SphereColour.EMPTY);
+		return boardRep[z][x][y] == SphereColour.EMPTY;
 	}
 	
 	private boolean isLocked(int x, int y, int z) {
@@ -37,21 +37,23 @@ public class PylosEnvironment {
 		int[] changeX = {0,0,-1,-1};
 		int[] changeY = {-1,0,0,-1};
 		boolean notLocked = true;
+		//check each of the possible four positions that could be on top of me
+		//that will cause me to be locked!
 		for(int i = 0; i < 4; i++) {
 			int cr,cc,cl;
 			cr = x+changeX[i];
 			cc = y+changeY[i];
 			cl = z+1;
+			//in the case of corner or edge squares the supposed locking position is
+			//off the board, so check if I'm in a valid position before trying
 			if(!PylosPosition.isValidPosition(cr,cc,cl))
 				continue;
 			try {
 				boolean isAboveEmpty = isEmpty(cr,cc,cl);
 				notLocked &= isAboveEmpty;
 			}
-			catch(IllegalArgumentException e) {
-				//this is inefficient, currently just checks at each of the four possible locations
-				//and throws an exception if it sees a bad argument
-				//then this will catch a bad argument and disregard it
+			catch(ArrayIndexOutOfBoundsException e) {
+				//just in case the check was bad, catch the exception and go on
 				continue;
 			}
 		}
@@ -76,13 +78,11 @@ public class PylosEnvironment {
 				if(!PylosPosition.isValidPosition(cr,cc,cl))
 					continue;
 				try {
-					boolean isBelowEmpty = isEmpty(cr,cc,cl);
-					canPlay &= isBelowEmpty;
+					boolean isBelowFilled = !isEmpty(cr,cc,cl);
+					canPlay &= isBelowFilled;
 				}
-				catch(IllegalArgumentException e) {
-					//this is inefficient, currently just checks at each of the four possible locations
-					//and throws an exception if it sees a bad argument
-					//then this will catch a bad argument and disregard it
+				catch(ArrayIndexOutOfBoundsException e) {
+					//just in case the check was bad, catch the exception and go on
 					continue;
 				}
 			}
@@ -91,7 +91,7 @@ public class PylosEnvironment {
 	}
 	
 	public void update(PylosMove m) throws Exception {
-		if(currentPlayer.equals(SphereColour.WHITE)) {
+		if(currentPlayer == SphereColour.WHITE) {
 			currentPlayer = SphereColour.BLACK;
 		}
 		else currentPlayer = SphereColour.WHITE;
--- src/edu/ai/SphereColour.java
+++ src/edu/ai/SphereColour.java
@@ -1,5 +1,15 @@
 package edu.ai;
 
-public enum SphereColour {
-	BLACK, WHITE, EMPTY
+//public enum SphereColour {
+//	BLACK, WHITE, EMPTY
+//}
+
+public class SphereColour {
+	static final int ILLEGAL = -2;
+	static final int UNSUPPORTED = -1;
+	static final int EMPTY = 0;
+	static final int WHITE = 1;
+	static final int BLACK = 2;
+	static final int WHITE_LOCKED = 3;	// Locked pieces are pieceColour*3
+	static final int BLACK_LOCKED = 6;
 }
