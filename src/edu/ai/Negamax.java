5975d14ed5e6b6a7614ef962070cf0534a362fdc
Joel Kalmanowicz <jetblackknight@gmail.com>
2012-05-28 16:03:04 -0700

    Had comparison wrong way around. Fixed some things.

--- src/edu/ai/Negamax.java
+++ src/edu/ai/Negamax.java
@@ -4,57 +4,43 @@ import java.util.*;
 
 
 public class Negamax extends PylosUtilityPlayer {
-  
-	private final int depthLimit;
-	private int depth;
 	
 	public Negamax(int c, IUtility u, int depthLimit) {
-		super(c, u);
-		this.depthLimit = depthLimit;
-	}
-		
-	@Override
-	public void updateMove(PylosMove m) {
-		e.update(m);
-		depth++;
+		super(c, u, depthLimit);
 	}
 
-	@Override
-	public void undoMove(PylosMove m) {
-		e.undoMove(m);
-		depth--;
-	}
-	
 	private double moveValue(PylosEnvironment e) {	
 		if ( e.isTerminal() || depth == depthLimit )
-			return u.getUtility(e, me);
-		// return the highest moveValue of successors
+			return u.getUtility(e, me) * (e.currentPlayer == me ? -1 : 1);
+		// If search ended on MY turn, OPPONENT had last move, and their utility is -(MY utility)
+		// But that will be taken care of by the inverted call to the function
+		// (Can't simplify because inverted call to function is necessary for nega)
 		double max = -1001;
 		double current = 0;
 		List<PylosMove> movelist = e.getMoves();
 		for (PylosMove x : movelist) {
-			updateMove(x);
+			doMove(x);
 			// Check utility of current move
-			current = moveValue(this.e);
-			if (max > current) {
+			current = -moveValue(this.e);
+			if (max < current) {
 				max = current;
 				// bestMove = x;	TODO: track best future moves to reduce redundant calculation
 			}
 			undoMove(x);
 		}
-		return e.currentPlayer == me ? max: -max;
+		return max;
 	}
 	
-	public PylosMove getMove() {
+	public PylosMove getMove() throws PylosUtilityException {
 		double max = -1001;
 		double current = 0;
 		PylosMove bestMove = null;
 		List<PylosMove> movelist = e.getMoves();
 		for (PylosMove x : movelist) {
-			updateMove(x);
+			doMove(x);
 			// Check utility of current move
 			current = moveValue(this.e);
-			if (max > current) {
+			if (max < current) {
 				max = current;
 				bestMove = x;
 			}
