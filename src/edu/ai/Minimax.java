0c87761459607f4db78736b1736b6e4f55ab835f
Joel Kalmanowicz <jetblackknight@gmail.com>
2012-05-27 20:56:07 -0700

    Most MiniMax structure done: just needs board cloning and testing

--- src/edu/ai/Minimax.java
+++ src/edu/ai/Minimax.java
@@ -1,38 +1,80 @@
-import java.util.*;
-
 package edu.ai;
 
-public class minimax {
+import java.util.*;
 
-  private int[] value;
-	
+
+public class Minimax extends PylosPlayer {
 	
 	// TODO: doMove function, accepts a PylosMove and a gameboard, returns ... uh
 	// TODO: fix PylosMove arguments and whatnot, a bit funky atm
+
+	private PylosEnvironment e;
+	private PylosColour me;
 	
-	public PylosMove decision(PylosEnvironment board, ArrayList<PylosMove> movelist) {
+	private int Utility(PylosEnvironment e) {
+		if(e.getWinner() == me) return 1;
+		else return -1;
+	}
+	
+	private int moveValue(PylosEnvironment e) {	
+		if ( e.isTerminal() ) {
+			return Utility(e);
+		}
+		else if(e.currentPlayer == me) {
+			// return the highest moveValue of Successors(state)
+			int max = 0;
+			int current = 0;
+			List<PylosMove> movelist = e.getMoves();
+			for (PylosMove x : movelist) {
+				// TODO: clone board
+				updateMove(x);
+				// Check utility of current move
+				current = moveValue(this.e);
+				if (max > current) {
+					max = current;
+					// bestMove = x;	TODO: track best future moves to reduce redundant calculation
+				}
+				undoMove(x);
+			}
+			return max;			
+		} else {
+			// return the lowest moveValue of Successors(state) (I.E. 
+			int min = 0;
+			int current = 0;
+			List<PylosMove> movelist = e.getMoves();
+			for (PylosMove x : movelist) {
+				// TODO: clone board
+				updateMove(x);
+				// Check utility of current move
+				current = moveValue(this.e);
+				if (min < current) {
+					min = current;
+					// bestMove = x;	TODO: track best future moves to reduce redundant calculation
+				}
+				undoMove(x);
+			}
+			return min;
+		}	
+	}
+	
+	public PylosMove getMove() {
 		int max = 0;
 		int current = 0;
 		PylosMove bestMove;
+		List<PylosMove> movelist = e.getMoves();
 		for (PylosMove x : movelist) {
-			current = moveValue(doMove(x, board), board);
+			// TODO: clone board
+			updateMove(x);
+			// Check utility of current move
+			current = moveValue(this.e);
 			if (max > current) {
 				max = current;
 				bestMove = x;
 			}
+			undoMove(x);
 		}
+		if(bestMove == null)
 		return bestMove;
 	}
-				
-	private moveValue(state, game) returns a utility value
-		if Terminal-Test[game](state) then
-			return Utility[game](state)
-		else if max is to move in state then
-			return the highest Minimax-Value of Successors(state)
-		else
-			return the lowest Minimax-Value of Successors(state)
-		
-		
-		}
-	
+					
 }
