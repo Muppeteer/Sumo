d89e17a7561bafa7ffd768f7e3e5a7a7954c50ac
Joel Kalmanowicz <jetblackknight@gmail.com>
2012-05-28 02:07:21 -0700

    I prefer "me" to colour ;P

--- src/edu/ai/PylosPlayer.java
+++ src/edu/ai/PylosPlayer.java
@@ -5,19 +5,19 @@ public abstract class PylosPlayer {
 
 	//internal game state representation
 	protected PylosEnvironment e;
-	protected PylosColour colour;
-	
+	protected PylosColour me;
+
 	public PylosPlayer(PylosColour c) {
 		e = new PylosEnvironment();
-		colour = c;
+		me = c;
 	}
-	
+
 	public abstract PylosMove getMove();
-	
+
 	public void updateMove(PylosMove m) {
 		e.update(m);
 	}
-	
+
 	public void undoMove(PylosMove m) {
 		e.undoMove(m);
 	}
