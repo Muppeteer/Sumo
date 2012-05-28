e7aaf8fe9899474ef6b6bee2784b354536875caf
Joel Kalmanowicz <jetblackknight@gmail.com>
2012-05-28 01:58:47 -0700

    Depth-limited, utility-based, exception-throwing MM madness

--- src/edu/ai/Minimax.java
+++ src/edu/ai/Minimax.java
@@ -5,44 +5,35 @@ import java.util.*;
 
 public class Minimax extends PylosUtilityPlayer {
 	
-	// TODO: doMove function, accepts a PylosMove and a gameboard, returns ... uh
-	// TODO: fix PylosMove arguments and whatnot, a bit funky atm
-
-//REVIEW: Oops. Didn't realise this was private. Made it protected so that any
-//player now inherits from PylosPlayer
-//	private PylosEnvironment e;
-//	private PylosColour colour;
+	private final int depthLimit;
+	private int depth;
 	
-	public Minimax(PylosColour c, Utility u) {
+	public Minimax(PylosColour c, Utility u, int depthLimit) {
 		super(c, u);
-		// TODO Auto-generated constructor stub
+		this.depthLimit = depthLimit;
+	}
+		
+	@Override
+	public void updateMove(PylosMove m) {
+		e.update(m);
+		depth++;
 	}
 
-//REVIEW: Suggestion that constructor signature become public Minimax(PylosColour c, Utility u)
-
-//REVIEW: Suggestion is that this is moved to an interface (Utility) with a single static method,
-//getUtility, and that Minimax is passed a Utility implementation during construction
-//this way, we keep our Utility evaluation entirely separate from our class
-	private int Utility(PylosEnvironment e) {
-		if(e.getWinner() == colour) return 1;
-		else return -1;
-//REVIEW:my suggestion is to get rid of this method entirely and use u.getCost(e)
+	@Override
+	public void undoMove(PylosMove m) {
+		e.undoMove(m);
+		depth--;
 	}
 	
-//REVIEW: This method searches until it hits a Terminal node? That might be somewhat
-//dangerous...should a depth be introduced now? Would be kinda simple?
-	private int moveValue(PylosEnvironment e) {	
-		if ( e.isTerminal() ) {
-//REVIEW: suggest that this becomes u.getCost(e)
-			return Utility(e);
-		}
-		else if(e.currentPlayer == colour) {
-			// return the highest moveValue of Successors(state)
-			int max = 0;
-			int current = 0;
+	private double moveValue(PylosEnvironment e) {	
+		if ( e.isTerminal() || depth == depthLimit)
+			return u.getUtility(e, me);
+		else if(e.currentPlayer == me) {
+			// return the highest moveValue of successors
+			double max = -1000;
+			double current = 0;
 			List<PylosMove> movelist = e.getMoves();
 			for (PylosMove x : movelist) {
-				// TODO: clone board
 				updateMove(x);
 				// Check utility of current move
 				current = moveValue(this.e);
@@ -54,12 +45,11 @@ public class Minimax extends PylosUtilityPlayer {
 			}
 			return max;			
 		} else {
-			// return the lowest moveValue of Successors(state) (I.E. 
-			int min = 0;
-			int current = 0;
+			// return the lowest moveValue of successors
+			double min = 0;
+			double current = 0;
 			List<PylosMove> movelist = e.getMoves();
 			for (PylosMove x : movelist) {
-				// TODO: clone board
 				updateMove(x);
 				// Check utility of current move
 				current = moveValue(this.e);
@@ -73,14 +63,12 @@ public class Minimax extends PylosUtilityPlayer {
 		}	
 	}
 	
-	public PylosMove getMove() {
-		int max = 0;
-		int current = 0;
-//REVIEW: bestMove should be initialised to null, just to be safe
+	public PylosMove getMove() throws PylosUtilityException {
+		double max = -1000;
+		double current = 0;
 		PylosMove bestMove = null;
 		List<PylosMove> movelist = e.getMoves();
 		for (PylosMove x : movelist) {
-			// TODO: clone board
 			updateMove(x);
 			// Check utility of current move
 			current = moveValue(this.e);
@@ -90,9 +78,7 @@ public class Minimax extends PylosUtilityPlayer {
 			}
 			undoMove(x);
 		}
-//REVIEW: why does this only return if bestMove is null? should that if-statement
-//have something inside it...?
-		if(bestMove == null)
+		if(bestMove == null) throw new PylosUtilityException(max);
 		return bestMove;
 	}
 					
