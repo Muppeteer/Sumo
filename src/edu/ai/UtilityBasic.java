3a89685aea3893590af9aa312ff5db90a19d5e35
Joel Kalmanowicz <jetblackknight@gmail.com>
2012-05-28 02:21:26 -0700

    So basic it's sic[k], bru

--- src/edu/ai/UtilityBasic.java
+++ src/edu/ai/UtilityBasic.java
@@ -1,5 +1,11 @@
 package edu.ai;
 
-public class UtilityBasic {
+public class UtilityBasic implements Utility {
+
+  @Override
+	public double getUtility(PylosEnvironment e, PylosColour c) {
+		if(e.isTerminal() && e.getWinner() == c) return 1000.0;
+		else return -1000.0;
+	}
 
 }
