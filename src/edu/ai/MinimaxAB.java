49db6c957f8f289267e8a1627328d69dc89cc5a3
Joel Kalmanowicz <jetblackknight@gmail.com>
2012-05-28 19:19:13 -0700

    I am the Alpha and the... Beta?

--- src/edu/ai/MinimaxAB.java
+++ src/edu/ai/MinimaxAB.java
@@ -1,5 +1,71 @@
 package edu.ai;
 
-public class MinimaxAB {
+import java.util.*;
 
+
+public class MinimaxAB extends Minimax {
+  
+	public MinimaxAB(int c, IUtility u, int depthLimit) {
+		super(c, u, depthLimit);
+	}
+
+	private double moveValue(PylosEnvironment e, double alpha, double beta) {	
+		if ( e.isTerminal() || depth == depthLimit)
+			return u.getUtility(e, me);
+		else if(e.currentPlayer == me) {
+			// return the highest moveValue of successors
+			double current = 0;
+			List<PylosMove> movelist = e.getMoves();
+			for (PylosMove x : movelist) {
+				doMove(x);
+				// Check utility of current move
+				current = moveValue(this.e, alpha, beta);
+				if (alpha < current) {
+					alpha = current;
+					// bestMove = x;	TODO: track best future moves to reduce redundant calculation
+				}
+				undoMove(x);
+				if (alpha >= beta) break;	// Beta prune
+			}
+			return alpha;			
+		} else {
+			// return the lowest moveValue of successors
+			double current = 0;
+			List<PylosMove> movelist = e.getMoves();
+			for (PylosMove x : movelist) {
+				doMove(x);
+				// Check utility of current move
+				current = moveValue(this.e, alpha, beta);
+				if (beta > current) {
+					beta = current;
+					// bestMove = x;	TODO: track best future moves to reduce redundant calculation
+				}
+				undoMove(x);
+				if (alpha >= beta) break;	// Alpha prune
+			}
+			return beta;
+		}	
+	}
+	
+	public PylosMove getMove() throws PylosUtilityException {
+		double current = 0;
+		double alpha = -1001;
+		double beta = 1001;
+		PylosMove bestMove = null;
+		List<PylosMove> movelist = e.getMoves();
+		for (PylosMove x : movelist) {
+			doMove(x);
+			// Check utility of current move
+			current = moveValue(this.e, alpha, beta);
+			if (alpha < current) {
+				alpha = current;
+				bestMove = x;
+			}
+			undoMove(x);
+			if (alpha >= beta) break;	// Beta prune
+		}
+		if(bestMove == null) throw new PylosUtilityException(alpha);
+		return bestMove;
+	}
+					
 }
